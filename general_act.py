074dbdc86d87a8271638909b70840d55db14239c
umarr <umarr.developer@gmail.com>
2023-12-18 09:44:03 +0600

    edit class GeneralAct

--- general_act.py
+++ general_act.py
@@ -1,24 +1,32 @@
 import pandas
+from alone_act import AloneAct
 
 
 class GeneralAct:
     name: str
-    data_: dict
+    acts: list[AloneAct]
+    data: pandas.DataFrame
 
     def __init__(self, name: str):
         self.name = name
-        self.data_ = {'column0': [], 'column1': []}
+        self.acts = []
 
-    def add_act(self, name: str, count: int):
+    def add_act(self, act: AloneAct):
         """
         Добавляет запись об актах
         """
-        self.data_['column0'].append(name)
-        self.data_['column1'].append(count)
+        self.acts.append(act)
+
+    def act_to_data(self):
+        data_ = {'column0': [], 'column1': []}
+        for act in self.acts:
+            data_['column0'].append(act.name)
+            data_['column1'].append(act.get_count())
+        self.data = pandas.DataFrame(data_)
 
     def write_act(self, path: str):
         """
         Записывает файлы с даннымии в указанныую папку
         """
-        data = pandas.DataFrame(self.data_)
-        data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
+        self.act_to_data()
+        self.data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
