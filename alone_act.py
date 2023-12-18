59bcb4bd0c15173b1ea6071be123d8c674585994
umarr <umarr.developer@gmail.com>
2023-12-18 11:17:54 +0600

    add class AloneActFormat

--- alone_act.py
+++ alone_act.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+import openpyxl
 import pandas
+from openpyxl.utils.dataframe import dataframe_to_rows
 
 
 class AloneAct:
@@ -34,4 +38,29 @@ class AloneAct:
         """
         Записывает файлы с даннымии в указанныую папку
         """
-        self.data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
+        format_act = AloneActFormat(self)
+        format_act.write_to_excel(path)
+        # self.data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
+
+
+class AloneActFormat:
+    template_folder = 'templates'
+    template_name = 'AloneActTemplate.xlsx'
+    sheet_name = 'Лист1'
+
+    def __init__(self, act: AloneAct):
+        self.act = act
+        self.workbook = openpyxl.load_workbook(f'{self.template_folder}/{self.template_name}')
+        self.sheet = self.workbook[self.sheet_name]
+
+    def write_to_excel(self, path: str):
+        date = datetime.now()
+
+        self.sheet['F3'] = f'{date.day}.{date.month}.{date.year}'
+        self.sheet['A6'] = f'{self.sheet["A6"].value} {self.act.name}'
+
+        for index, row in enumerate(dataframe_to_rows(self.act.data, index=False, header=False), start=1):
+            row.insert(0, index)
+            self.sheet.append(row)
+
+        self.workbook.save(f'{path}/{self.act.name}.xlsx')
