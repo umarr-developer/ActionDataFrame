6e0db57f8814c63b38f27e01247a53309f1c2d4e
umarr <umarr.developer@gmail.com>
2023-12-21 12:14:02 +0600

    add func 'set_print_area' and 'set_values'

--- alone_act.py
+++ alone_act.py
@@ -11,15 +11,17 @@ class AloneAct:
     filter_column: str
     columns: list[str]
     data: pandas.DataFrame
+    signature: bool
     count: int
 
     def __init__(self, name: str, branches: list[str], filter_column: str, columns: list[str],
-                 all_data: pandas.DataFrame):
+                 all_data: pandas.DataFrame, signature: bool = True):
         self.name = name
         self.branches = branches
         self.filter_column = filter_column
         self.columns = columns
         self.data = self.filter_act(all_data)
+        self.signature = signature
         self.count = len(self.data)
 
     def filter_act(self, all_data: pandas.DataFrame):
@@ -46,22 +48,48 @@ class AloneAct:
 
 class AloneActFormat:
     template_folder = 'templates'
-    template_name = 'AloneActTemplate.xlsx'
+    template_name = 'AloneActTemplate.xlsm'
     sheet_name = 'Лист1'
+    start_pos = 7
 
     def __init__(self, act: AloneAct):
         self.act = act
         self.workbook = openpyxl.load_workbook(f'{self.template_folder}/{self.template_name}')
         self.sheet = self.workbook[self.sheet_name]
 
-    def write_to_excel(self, path: str):
-        date = datetime.now()
+    def set_print_area(self, rows_count: int, signature: bool):
+        index = self.start_pos + rows_count
+        if signature:
+            index += 10
+        self.sheet.page_setup.paperSize = self.sheet.PAPERSIZE_A4
+        self.sheet.print_area = f'A1:F{index}'
 
+    def set_values(self, rows_count: int, signature: bool):
+        date = datetime.now()
         self.sheet['F3'] = f'{date.day}.{date.month}.{date.year}'
         self.sheet['A6'] = f'{self.sheet["A6"].value} {self.act.name}'
 
+        if signature:
+            index = self.start_pos + rows_count + 3
+
+            self.sheet[f'B{index}'] = 'Передал сотрудник ОЭиПК:'
+            self.sheet[f'B{index + 3}'] = 'Передал: '
+            self.sheet[f'B{index + 4}'] = 'Выездной сотрудник ОЛ '
+            self.sheet[f'B{index + 5}'] = '(ФИО и подпись)'
+
+            self.sheet[f'D{index}'] = 'Принял Сотрудник ОЛ:'
+            self.sheet[f'D{index + 1}'] = '(ФИО и подпись)'
+            self.sheet[f'D{index + 3}'] = 'Принял: '
+            self.sheet[f'D{index + 4}'] = 'Нач ОО/зав с/к'
+            self.sheet[f'D{index + 5}'] = '(ФИО и подпись)'
+
+    def write_to_excel(self, path: str):
+        self.set_print_area(self.act.count, self.act.signature)
+        self.set_values(self.act.count, self.act.signature)
+
         for index, row in enumerate(dataframe_to_rows(self.act.data, index=False, header=False), start=1):
             row.insert(0, index)
-            self.sheet.append(row)
+            for index_, value in enumerate(row):
+                self.sheet.cell(self.start_pos + index, index_ + 1, value)
 
         self.workbook.save(f'{path}/{self.act.name}.xlsx')
