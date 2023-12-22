43519b0cf32fe90d60ec2e5f397c16e8f64ba238
umarr <umarr.developer@gmail.com>
2023-12-22 11:57:02 +0600

    add class GeneralActTemplate

--- general_act.py
+++ general_act.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
+import openpyxl
 import pandas
+from openpyxl.utils.dataframe import dataframe_to_rows
+
 from alone_act import AloneAct
 
 
@@ -29,4 +34,58 @@ class GeneralAct:
         Записывает файлы с даннымии в указанныую папку
         """
         self.act_to_data()
-        self.data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
+        format_act = GeneralActFormat(self)
+        format_act.write_to_excel(path)
+        # self.data.to_excel(f'{path}/{self.name}.xlsx', index=False, header=False)
+
+
+class GeneralActFormat:
+    template_folder = 'templates'
+    template_name = 'GeneralActTemplate.xlsm'
+    sheet_name = 'Лист1'
+    start_pos = 10
+
+    def __init__(self, act: GeneralAct):
+        self.act = act
+        self.workbook = openpyxl.load_workbook(f'{self.template_folder}/{self.template_name}')
+        self.sheet = self.workbook[self.sheet_name]
+
+    def set_print_area(self, rows_count: int, alt_index: int):
+        index = self.start_pos + rows_count
+
+        self.sheet.page_setup.paperSize = self.sheet.PAPERSIZE_A4
+        self.sheet.print_area = f'A1:C{index + alt_index}'
+
+    def set_values(self):
+        rows_count = len(self.act.acts)
+        alt_index = 10
+
+        date = datetime.now()
+        self.sheet['C3'] = f'{date.day}.{date.month}.{date.year}'
+
+        index = self.start_pos + rows_count + 3
+
+        self.sheet[f'A{index}'] = 'Передал сотрудник ОЭиПК:'
+        self.sheet[f'A{index + 1}'] = 'Абдрахманова Г.М.'
+        self.sheet[f'A{index + 2}'] = 'Кайып уулу Э.'
+        self.sheet[f'A{index + 3}'] = 'Передал:'
+        self.sheet[f'A{index + 4}'] = 'Выездной сотрудник ОЛ'
+        self.sheet[f'A{index + 5}'] = '(ФИО и подпись)'
+
+        self.sheet[f'C{index}'] = 'Принял Сотрудник ОЛ:'
+        self.sheet[f'C{index + 1}'] = '(ФИО и подпись)'
+        self.sheet[f'C{index + 3}'] = 'Принял: '
+        self.sheet[f'C{index + 4}'] = 'Нач ОО/зав с/к'
+        self.sheet[f'C{index + 5}'] = '(ФИО и подпись)'
+
+        self.set_print_area(rows_count, alt_index)
+
+    def write_to_excel(self, path: str):
+        self.set_values()
+
+        for index, row in enumerate(dataframe_to_rows(self.act.data, index=False, header=False), start=1):
+            row.insert(0, index)
+            for index_, value in enumerate(row):
+                self.sheet.cell(self.start_pos + index, index_ + 1, value)
+
+        self.workbook.save(f'{path}/{self.act.name}.xlsx')
